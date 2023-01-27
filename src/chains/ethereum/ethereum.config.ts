ecde46d09133a46253c4a878be2cc69afd858352
Michael Feng <mifeng@gmail.com>
2023-01-26 20:56:23 -0800

    (feat) initial commit from hummingbot repo

---
+++ src/chains/ethereum/ethereum.config.ts
@@ -0,0 +1,60 @@
+import { TokenListType } from '../../services/base';
+import { ConfigManagerV2 } from '../../services/config-manager-v2';
+export interface NetworkConfig {
+  name: string;
+  chainID: number;
+  nodeURL: string;
+  tokenListType: TokenListType;
+  tokenListSource: string;
+  gasPriceRefreshInterval: number | undefined;
+}
+
+export interface EthereumGasStationConfig {
+  enabled: boolean;
+  gasStationURL: string;
+  APIKey: string;
+  gasLevel: string;
+}
+
+export interface Config {
+  network: NetworkConfig;
+  nativeCurrencySymbol: string;
+  manualGasPrice: number;
+  gasLimitTransaction: number;
+}
+
+export function getEthereumConfig(
+  chainName: string,
+  networkName: string
+): Config {
+  const network = networkName;
+  return {
+    network: {
+      name: network,
+      chainID: ConfigManagerV2.getInstance().get(
+        chainName + '.networks.' + network + '.chainID'
+      ),
+      nodeURL: ConfigManagerV2.getInstance().get(
+        chainName + '.networks.' + network + '.nodeURL'
+      ),
+      tokenListType: ConfigManagerV2.getInstance().get(
+        chainName + '.networks.' + network + '.tokenListType'
+      ),
+      tokenListSource: ConfigManagerV2.getInstance().get(
+        chainName + '.networks.' + network + '.tokenListSource'
+      ),
+      gasPriceRefreshInterval: ConfigManagerV2.getInstance().get(
+        chainName + '.networks.' + network + '.gasPriceRefreshInterval'
+      ),
+    },
+    nativeCurrencySymbol: ConfigManagerV2.getInstance().get(
+      chainName + '.networks.' + network + '.nativeCurrencySymbol'
+    ),
+    manualGasPrice: ConfigManagerV2.getInstance().get(
+      chainName + '.manualGasPrice'
+    ),
+    gasLimitTransaction: ConfigManagerV2.getInstance().get(
+      chainName + '.gasLimitTransaction'
+    ),
+  };
+}
