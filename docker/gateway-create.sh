45d9746e5db72d8ba8395d5f8e76c33cab5b5256
dardonacci <36869960+cardosofede@users.noreply.github.com>
2023-02-23 12:16:45 -0300

    Update gateway-create.sh
    Fix merge issues

--- docker/gateway-create.sh
+++ docker/gateway-create.sh
@@ -154,12 +154,8 @@ create_instance () {
    # Launch a new instance of gateway
    docker run \
    --name $INSTANCE_NAME \
-<<<<<<< HEAD
    -p $GATEWAY_PORT:15888 \
    -p $DOCS_PORT:8080 \
-=======
-   -p $PORT:$PORT \
->>>>>>> 7c0751792bb043fe6d594596e21d66a43f90ab98
    -v $CONF_FOLDER:/usr/src/app/conf \
    -v $LOGS_FOLDER:/usr/src/app/logs \
    -v $CERTS_FOLDER:/usr/src/app/certs \
