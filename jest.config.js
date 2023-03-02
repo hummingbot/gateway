eb9a6d6fe12f2e3cb43cb01bc0c0dd7f46d1457d
vic-en <31972210+vic-en@users.noreply.github.com>
2023-03-01 21:54:27 -0600

    minor update

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,7 @@ module.exports = {
     'src/chains/avalanche/avalanche.ts',
     'src/chains/avalanche/pangolin/pangolin.ts',
     'src/chains/cosmos/cosmos.ts',
+    'src/chains/injective/injective.ts',
     'src/chains/near/near.ts',
     'src/chains/near/near.base.ts',
     'src/connectors/uniswap/uniswap.config.ts',
