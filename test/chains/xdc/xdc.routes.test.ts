25f55d310df457e5a30ce47fa3866de46919c46b
Michael Feng <mifeng@gmail.com>
2023-03-16 15:38:57 -0700

    (fix) change to EIP 1559 for gas calcs

--- test/chains/xdc/xdc.routes.test.ts
+++ test/chains/xdc/xdc.routes.test.ts
@@ -3,10 +3,8 @@ import { patch, unpatch } from '../../services/patch';
 import { gatewayApp } from '../../../src/app';
 import {
   NETWORK_ERROR_CODE,
-  OUT_OF_GAS_ERROR_CODE,
   UNKNOWN_ERROR_ERROR_CODE,
   NETWORK_ERROR_MESSAGE,
-  OUT_OF_GAS_ERROR_MESSAGE,
   UNKNOWN_ERROR_MESSAGE,
 } from '../../../src/services/error-handler';
 import * as transactionSuccesful from '../ethereum/fixtures/transaction-succesful.json';
@@ -371,22 +369,6 @@ describe('POST /network/poll', () => {
 
     expect(res.statusCode).toEqual(503);
     expect(res.body.errorCode).toEqual(UNKNOWN_ERROR_ERROR_CODE);
-  });
-
-  it('should get an OUT of GAS error for failed out of gas transactions', async () => {
-    patch(xdc, 'getCurrentBlockNumber', () => 1);
-    patch(xdc, 'getTransaction', () => transactionOutOfGas);
-    patch(xdc, 'getTransactionReceipt', () => transactionOutOfGasReceipt);
-    const res = await request(gatewayApp).post('/network/poll').send({
-      chain: 'xdc',
-      network: 'apothem',
-      txHash:
-        '0x2faeb1aa55f96c1db55f643a8cf19b0f76bf091d0b7d1b068d2e829414576362', // noqa: mock
-    });
-
-    expect(res.statusCode).toEqual(503);
-    expect(res.body.errorCode).toEqual(OUT_OF_GAS_ERROR_CODE);
-    expect(res.body.message).toEqual(OUT_OF_GAS_ERROR_MESSAGE);
   });
 
   it('should get a null in txReceipt for Tx in the mempool', async () => {
