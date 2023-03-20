26af3b894daea1c6296ceefc84715cecfae85f24
James <james@hummingbot.io>
2023-03-20 18:52:13 +0100

    Add some utility functions for zigzag

--- src/connectors/zigzag/zigzag.ts
+++ src/connectors/zigzag/zigzag.ts
@@ -1,3 +1,4 @@
+// import {BigNumber} from 'ethers';
 import LRUCache from 'lru-cache';
 import { ZigZagConfig } from './zigzag.config';
 import axios from 'axios';
@@ -16,20 +17,154 @@ export interface Token {
   name: string;
 }
 
-// export interface Market {
-//   market: string;
-//   baseSymbol: string;
-//   quoteSymbol: string;
-//   lastPrice: number;
-//   lowestAsk: number;
-//   highestBid: number;
-//   baseVolume: number;
-//   quoteVolume: number;
-//   priceChange: number;
-//   priceChangePercent_24h: number;
-//   highestPrice_24h: number;
-//   lowestPrice_24h: number;
-//   numberOfTrades_24h: number;
+export interface Market {
+  market: string;
+  baseSymbol: string;
+  quoteSymbol: string;
+  lastPrice: number;
+  lowestAsk: number;
+  highestBid: number;
+  baseVolume: number;
+  quoteVolume: number;
+  priceChange: number;
+  priceChangePercent_24h: number;
+  highestPrice_24h: number;
+  lowestPrice_24h: number;
+  numberOfTrades_24h: number;
+}
+
+export type RouteMarket = {
+  buyTokenAddress: string;
+  sellTokenAddress: string;
+};
+
+export type ZZOrder = {
+  order: {
+    user: string;
+    buyToken: string;
+    sellToken: string;
+    buyAmount: string;
+    sellAmount: string;
+    expirationTimeSeconds: string;
+  };
+  signature: string;
+};
+
+// const parsedMarkets = [
+//   `${ethers.constants.AddressZero}-${network.wethContractAddress}`,
+//   `${network.wethContractAddress}-${ethers.constants.AddressZero}`
+// ]
+
+// const response = await fetch(`${network.backendUrl}/v1/info`)
+// if (response.status !== 200) {
+//   console.error("fetchMarketsInfo: Failed to fetch market info.")
+//   return
+// }
+
+// result = await response.json()
+// result.markets.forEach(market => {
+//   if (!market.verified) return
+
+//   const parsedBuyToken = market.buyToken.toLowerCase()
+//   const parsedSellToken = market.sellToken.toLowerCase()
+//   parsedMarkets.push(`${parsedBuyToken}-${parsedSellToken}`)
+
+//   // add ETH version of market
+//   if (network.wethContractAddress === parsedBuyToken) {
+//     parsedMarkets.push(`${ethers.constants.AddressZero}-${parsedSellToken}`)
+//   } else if (network.wethContractAddress === parsedSellToken) {
+//     parsedMarkets.push(`${parsedBuyToken}-${ethers.constants.AddressZero}`)
+//   }
+// })
+
+// if (network.wethContractAddress) {
+//   // add wrap/unwrap
+//   parsedMarkets.push(`${ethers.constants.AddressZero}-${network.wethContractAddress}`)
+//   parsedMarkets.push(`${network.wethContractAddress}-${ethers.constants.AddressZero}`)
+// }
+
+export async function getOrderBook(
+  possibleRoutes: RouteMarket[][]
+): Promise<{ [key: string]: ZZOrder[] }> {
+  const minExpires = (Date.now() / 1000 + 11).toFixed(0);
+  const minTimeStamp: number = Date.now() / 1000 + 10;
+  let orders: { orders: ZZOrder[] };
+  const newOrderBook: { [key: string]: ZZOrder[] } = {};
+
+  const promise0 = possibleRoutes.map(async (routes: RouteMarket[]) => {
+    const promise1 = routes.map(async (market: RouteMarket) => {
+      const requestSellTokenAddress = market.sellTokenAddress;
+      const requestBuyTokenAddress = market.buyTokenAddress;
+      const response = await fetch(
+        `https://zigzag-exchange.herokuapp.com/v1/orders?buyToken=${requestBuyTokenAddress}&sellToken=${requestSellTokenAddress}&minExpires=${minExpires}`
+      );
+
+      orders = await response.json();
+      const goodOrders = orders.orders.filter(
+        (o: ZZOrder) => minTimeStamp < Number(o.order.expirationTimeSeconds)
+      );
+      const key = `${requestBuyTokenAddress}-${requestSellTokenAddress}`;
+      newOrderBook[key] = goodOrders;
+    });
+    await Promise.all(promise1);
+  });
+  await Promise.all(promise0);
+
+  return newOrderBook;
+}
+
+export function getPossibleRoutes(
+  markets: Array<string>,
+  sellToken: Token,
+  buyToken: Token
+): RouteMarket[][] {
+  let newRoute: RouteMarket[][] = [];
+  const tradeMarket = `${sellToken.address}-${buyToken.address}`;
+
+  if (markets.includes(tradeMarket)) {
+    newRoute.push([
+      {
+        buyTokenAddress: sellToken.address,
+        sellTokenAddress: buyToken.address,
+      },
+    ]);
+  }
+
+  // check routes via other tokens
+  if (newRoute.length === 0) {
+    const possibleRoutes = [
+      '0x82af49447d8a07e3bd95bd0d56f35241523fbab1', // weth
+      '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8', // usdc
+      '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9', // usdt
+    ];
+    possibleRoutes.forEach((routeTokenAddress) => {
+      const firstTradeMarket = `${sellToken.address}-${routeTokenAddress}`;
+      const secondTradeMarket = `${routeTokenAddress}-${buyToken.address}`;
+      if (
+        markets.includes(firstTradeMarket) &&
+        markets.includes(secondTradeMarket)
+      ) {
+        newRoute.push([
+          {
+            buyTokenAddress: sellToken.address,
+            sellTokenAddress: routeTokenAddress,
+          },
+          {
+            buyTokenAddress: routeTokenAddress,
+            sellTokenAddress: buyToken.address,
+          },
+        ]);
+      }
+    });
+  }
+
+  return newRoute;
+}
+
+// function estimate(sellAmount: BigNumber, buyAmount: BigNumber) {
+//   let newQuoteOrderArray: ZZOrder[] = [];
+//   let newSwapPrice: number = 0;
+//   let bestSwapRoute: RouteMarket[] = [];
 // }
 
 export class ZigZag {
@@ -61,7 +196,7 @@ export class ZigZag {
 
   public async loadMarkets() {
     this.parsedMarkets = await axios.get(
-      'https://zigzag-exchange.herokuapp.com/api/v1/markets/1'
+      'https://zigzag-exchange.herokuapp.com/api/v1/markets'
     );
   }
 
@@ -85,25 +220,37 @@ export class ZigZag {
     return this._ready;
   }
 
-  public getTokenByAddress(address: string): Token {
-    return this.tokenList[address];
-  }
-  
-  public getTokenInfo(tokenSymbol: string) {
+  // public getTokenByAddress(address: string): Token {
+  //   return this.tokenList[address];
+  // }
 
-    // https://api.zksync.io/api/v0.2/tokens/USDC
-    // https://api.zksync.io/api/v0.2/tokens/WETH
-    
-    const tokenInfo = await axios.get(`https://api.zksync.io/api/v0.2/tokens/${token}`);
-    const priceInfo = await axios.get(
-            `https://api.zksync.io/api/v0.2/tokens/${token}/priceIn/usd`
-    );
+  // public getTokenInfo(token: string) {
 
-    0xf4037f59c92c9893c43c2372286699430310cfe7
+  //   // https://api.zksync.io/api/v0.2/tokens/USDC
+  //   // https://api.zksync.io/api/v0.2/tokens/WETH
 
-    console.log(tokenInfo);
-    console.log(priceInfo);
-  }
+  //   // const tokenInfo = await axios.get(`https://api.zksync.io/api/v0.2/tokens/${token}`);
+  //   // // id, address, symbol, decimals
+  //   // const priceInfo = await axios.get(
+  //   //         `https://api.zksync.io/api/v0.2/tokens/${token}/priceIn/usd`
+  //   // );
+
+  //   // // 0xf4037f59c92c9893c43c2372286699430310cfe7
+
+  //   // console.log(tokenInfo);
+  //   // console.log(priceInfo);
+  // }
+
+  // async estimateSellTrade(
+  //   baseToken: string,
+  //   quoteToken: string,
+  //   amount: BigNumber,
+  //   allowedSlippage?: string
+  // ) {
+
+  //   // check if market is active
+
+  // }
 
   // public async markets(
   //   req
@@ -157,9 +304,6 @@ GET
 
 
 */
-
-
-
 
 /*
 
