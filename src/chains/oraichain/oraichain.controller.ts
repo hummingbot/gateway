c2bfafc12654567c9c5449b5c8d001355dd715b3
trung2891 <trungnv280201@gmail.com>
2024-01-30 16:03:59 +0700

    feat: impl controller for oraichain

--- src/chains/oraichain/oraichain.controller.ts
+++ src/chains/oraichain/oraichain.controller.ts
@@ -1,11 +1,28 @@
 import { TokensRequest } from '../../network/network.requests';
 import { TokenInfo } from '../../services/base';
-import { CosmosController } from '../cosmos/cosmos.controllers';
+import {
+  // CosmosController,
+  toCosmosBalances,
+} from '../cosmos/cosmos.controllers';
 import { Token } from '../cosmos/cosmos-base';
 import { Oraichain } from './oraichain';
 import { validateGetTokensRequest } from './oraichain.validators';
+import {
+  validateCosmosBalanceRequest,
+  validateCosmosPollRequest,
+} from '../cosmos/cosmos.validators';
+import {
+  CosmosBalanceRequest,
+  CosmosPollRequest,
+} from '../cosmos/cosmos.requests';
+import {
+  HttpException,
+  TOKEN_NOT_SUPPORTED_ERROR_CODE,
+  TOKEN_NOT_SUPPORTED_ERROR_MESSAGE,
+} from '../../services/error-handler';
+import { decodeTxRaw } from '@cosmjs/proto-signing';
 
-export class OraichainController extends CosmosController {
+export class OraichainController {
   static async getTokens(
     oraichainLish: Oraichain,
     req: TokensRequest
@@ -38,5 +55,48 @@ export class OraichainController extends CosmosController {
     });
 
     return { tokens: tokensInfo };
+  }
+
+  static async balances(cosmosish: Oraichain, req: CosmosBalanceRequest) {
+    validateCosmosBalanceRequest(req);
+
+    const wallet = await cosmosish.getWallet(req.address, 'orai');
+
+    const { tokenSymbols } = req;
+
+    tokenSymbols.forEach((symbol: string) => {
+      const token = cosmosish.getTokenForSymbol(symbol);
+
+      if (!token) {
+        throw new HttpException(
+          500,
+          TOKEN_NOT_SUPPORTED_ERROR_MESSAGE + symbol,
+          TOKEN_NOT_SUPPORTED_ERROR_CODE
+        );
+      }
+    });
+
+    const balances = await cosmosish.getBalances(wallet);
+    const filteredBalances = toCosmosBalances(balances, tokenSymbols);
+
+    return {
+      balances: filteredBalances,
+    };
+  }
+
+  static async poll(cosmos: Oraichain, req: CosmosPollRequest) {
+    validateCosmosPollRequest(req);
+
+    const transaction = await cosmos.getTransaction(req.txHash);
+    const currentBlock = await cosmos.getCurrentBlockNumber();
+
+    return {
+      txHash: req.txHash,
+      currentBlock,
+      txBlock: transaction.height,
+      gasUsed: transaction.gasUsed,
+      gasWanted: transaction.gasWanted,
+      txData: decodeTxRaw(transaction.tx),
+    };
   }
 }
