c50671a106804777fd21eafa71ccd73966cc034e
elijahsnoz <elijahsnoz@gmail.com>
2024-04-23 00:36:33 +0100

    (fix) update uniswap packages

--- src/services/connection-manager.ts
+++ src/services/connection-manager.ts
@@ -191,14 +191,14 @@ export async function getConnector<T>(
   let connectorInstance: ConnectorUnion;
 
   if (
-    (chain === 'ethereum' || chain === 'polygon' || chain === "celo") &&
+    (chain === 'ethereum' || chain === 'polygon') &&
     connector === 'uniswap'
   ) {
     connectorInstance = Uniswap.getInstance(chain, network);
   } else if (chain === 'polygon' && connector === 'quickswap') {
     connectorInstance = Quickswap.getInstance(chain, network);
   } else if (
-    (chain === 'ethereum' || chain === 'polygon' || chain === "celo") &&
+    (chain === 'ethereum' || chain === 'polygon') &&
     connector === 'uniswapLP'
   ) {
     connectorInstance = UniswapLP.getInstance(chain, network);
