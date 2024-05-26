cd4a4dbb0525375202969db55d8ee68b844db88d
harjas27 <harjasbathla@gmail.com>
2024-05-26 23:23:11 +0530

    routes tests

---
+++ test/connectors/uniswap/bsc.uniswap.routes.test.ts
@@ -0,0 +1,678 @@
+import express from 'express';
+import { Express } from 'express-serve-static-core';
+import request from 'supertest';
+import { Uniswap } from '../../../src/connectors/uniswap/uniswap';
+import { AmmRoutes } from '../../../src/amm/amm.routes';
+import { patch, unpatch } from '../../../test/services/patch';
+import { gasCostInEthString } from '../../../src/services/base';
+import { patchEVMNonceManager } from '../../evm.nonce.mock';
+import {BinanceSmartChain} from "../../../src/chains/binance-smart-chain/binance-smart-chain";
+let app: Express;
+let binanceSmartChain: BinanceSmartChain;
+let uniswap: Uniswap;
+
+beforeAll(async () => {
+  app = express();
+  app.use(express.json());
+
+  binanceSmartChain = BinanceSmartChain.getInstance('mainnet');
+  patchEVMNonceManager(binanceSmartChain.nonceManager);
+  await binanceSmartChain.init();
+
+  uniswap = Uniswap.getInstance('binance-smart-chain', 'mainnet');
+  await uniswap.init();
+
+  app.use('/amm', AmmRoutes.router);
+});
+
+beforeEach(() => {
+  patchEVMNonceManager(binanceSmartChain.nonceManager);
+});
+
+afterEach(() => {
+  unpatch();
+});
+
+afterAll(async () => {
+  await binanceSmartChain.close();
+});
+
+const address: string = '0x242532ebDfcc760f2Ddfe8378eB51f5F847CE5bD';
+
+const patchGetWallet = () => {
+  patch(binanceSmartChain, 'getWallet', () => {
+    return {
+      address: address,
+    };
+  });
+};
+
+const patchInit = () => {
+  patch(uniswap, 'init', async () => {
+    return;
+  });
+};
+
+const patchStoredTokenList = () => {
+  patch(binanceSmartChain, 'tokenList', () => {
+    return [
+      {
+        chainId: 56,
+        name: 'WBNB',
+        symbol: 'WBNB',
+        address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+        decimals: 18,
+      },
+      {
+        chainId: 56, 
+        name: 'DAI', 
+        symbol: 'DAI',
+        address: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
+        decimals: 18,
+      },
+    ];
+  });
+};
+
+const patchGetTokenBySymbol = () => {
+  patch(binanceSmartChain, 'getTokenBySymbol', (symbol: string) => {
+    if (symbol === 'WBNB') {
+      return {
+        chainId: 56,
+        name: 'WBNB',
+        symbol: 'WBNB',
+        address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+        decimals: 18,
+      };
+    } else {
+      return {
+        chainId: 56,
+        name: 'DAI',
+        symbol: 'DAI',
+        address: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
+        decimals: 18,
+      };
+    }
+  });
+};
+
+const patchGetTokenByAddress = () => {
+  patch(binanceSmartChain, 'getTokenByAddress', () => {
+    return {
+      chainId: 56,
+      name: 'WBNB',
+      symbol: 'WBNB',
+      address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+      decimals: 18,
+    };
+  });
+};
+
+const patchGasPrice = () => {
+  patch(binanceSmartChain, 'gasPrice', () => 100);
+};
+
+const patchEstimateBuyTrade = () => {
+    patch(uniswap, 'estimateBuyTrade', () => {
+        return {
+            expectedAmount: {
+                toSignificant: () => 100,
+            },
+            trade: {
+                executionPrice: {
+                    invert: jest.fn().mockReturnValue({
+                        toSignificant: () => 100,
+                        toFixed: () => '100',
+                    }),
+                },
+            },
+        };
+    });
+};
+
+const patchEstimateSellTrade = () => {
+    patch(uniswap, 'estimateSellTrade', () => {
+        return {
+            expectedAmount: {
+                toSignificant: () => 100,
+            },
+            trade: {
+                executionPrice: {
+                    toSignificant: () => 100,
+                    toFixed: () => '100',
+                },
+            },
+        };
+    });
+};
+
+const patchGetNonce = () => {
+  patch(binanceSmartChain.nonceManager, 'getNonce', () => 100);
+};
+
+const patchExecuteTrade = () => {
+  patch(uniswap, 'executeTrade', () => {
+    return { nonce: 100, hash: '000000000000000' };
+  });
+};
+
+describe('POST /amm/price', () => {
+  it('should return 200 for BUY', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+    patchGasPrice();
+    patchEstimateBuyTrade();
+    patchGetNonce();
+    patchExecuteTrade();
+
+    await request(app)
+      .post(`/amm/price`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        side: 'BUY',
+      })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((res: any) => {
+        expect(res.body.amount).toEqual('10000.000000000000000000');
+        expect(res.body.rawAmount).toEqual('10000000000000000000000');
+      });
+  });
+
+  it('should return 200 for SELL', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+    patchGasPrice();
+    patchEstimateSellTrade();
+    patchGetNonce();
+    patchExecuteTrade();
+
+    await request(app)
+      .post(`/amm/price`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        side: 'SELL',
+      })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((res: any) => {
+        expect(res.body.amount).toEqual('10000.000000000000000000');
+        expect(res.body.rawAmount).toEqual('10000000000000000000000');
+      });
+  });
+
+  it('should return 500 for unrecognized quote symbol', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+
+    await request(app)
+      .post(`/amm/price`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DOGE',
+        base: 'WBNB',
+        amount: '10000',
+        side: 'SELL',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+
+  it('should return 500 for unrecognized base symbol', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+
+    await request(app)
+      .post(`/amm/price`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'SHIBA',
+        amount: '10000',
+        side: 'SELL',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+
+  it('should return 500 for unrecognized base symbol with decimals in the amount and SELL', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+
+    await request(app)
+      .post(`/amm/price`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'SHIBA',
+        amount: '10.000',
+        side: 'SELL',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+
+  it('should return 500 for unrecognized base symbol with decimals in the amount and BUY', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+
+    await request(app)
+      .post(`/amm/price`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'SHIBA',
+        amount: '10.000',
+        side: 'BUY',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+
+  it('should return 500 when the priceSwapIn operation fails', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+    patch(uniswap, 'priceSwapIn', () => {
+      return 'error';
+    });
+
+    await request(app)
+      .post(`/amm/price`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DOGE',
+        base: 'WBNB',
+        amount: '10000',
+        side: 'SELL',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+
+  it('should return 500 when the priceSwapOut operation fails', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+    patch(uniswap, 'priceSwapOut', () => {
+      return 'error';
+    });
+
+    await request(app)
+      .post(`/amm/price`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DOGE',
+        base: 'WBNB',
+        amount: '10000',
+        side: 'BUY',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+});
+
+describe('POST /amm/trade', () => {
+  const patchForBuy = () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+    patchGasPrice();
+    patchEstimateBuyTrade();
+    patchGetNonce();
+    patchExecuteTrade();
+  };
+  it('should return 200 for BUY', async () => {
+    patchForBuy();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'BUY',
+        nonce: 21,
+      })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((res: any) => {
+        expect(res.body.nonce).toEqual(21);
+      });
+  });
+
+  it('should return 200 for BUY without nonce parameter', async () => {
+    patchForBuy();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'BUY',
+      })
+      .set('Accept', 'application/json')
+      .expect(200);
+  });
+
+  it('should return 200 for BUY with maxFeePerGas and maxPriorityFeePerGas', async () => {
+    patchForBuy();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'BUY',
+        nonce: 21,
+        maxFeePerGas: '5000000000',
+        maxPriorityFeePerGas: '5000000000',
+      })
+      .set('Accept', 'application/json')
+      .expect(200);
+  });
+
+  const patchForSell = () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+    patchGasPrice();
+    patchEstimateSellTrade();
+    patchGetNonce();
+    patchExecuteTrade();
+  };
+  it('should return 200 for SELL', async () => {
+    patchForSell();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'SELL',
+        nonce: 21,
+      })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((res: any) => {
+        expect(res.body.nonce).toEqual(21);
+      });
+  });
+
+  it('should return 200 for SELL  with maxFeePerGas and maxPriorityFeePerGas', async () => {
+    patchForSell();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'SELL',
+        nonce: 21,
+        maxFeePerGas: '5000000000',
+        maxPriorityFeePerGas: '5000000000',
+      })
+      .set('Accept', 'application/json')
+      .expect(200);
+  });
+
+  it('should return 200 for SELL with limitPrice', async () => {
+    patchForSell();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'SELL',
+        nonce: 21,
+        limitPrice: '9',
+      })
+      .set('Accept', 'application/json')
+      .expect(200);
+  });
+
+  it('should return 200 for BUY with limitPrice', async () => {
+    patchForBuy();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'BUY',
+        nonce: 21,
+        limitPrice: '999999999999999999999',
+      })
+      .set('Accept', 'application/json')
+      .expect(200);
+  });
+
+  it('should return 500 for BUY with price smaller than limitPrice', async () => {
+    patchForBuy();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'BUY',
+        nonce: 21,
+        limitPrice: '9',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+
+  it('should return 500 for SELL with price higher than limitPrice', async () => {
+    patchForSell();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'SELL',
+        nonce: 21,
+        limitPrice: '99999999999',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+
+  it('should return 404 when parameters are incorrect', async () => {
+    patchInit();
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: 10000,
+        address: 'da8',
+        side: 'comprar',
+      })
+      .set('Accept', 'application/json')
+      .expect(404);
+  });
+  it('should return 500 when the priceSwapIn operation fails', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+    patch(uniswap, 'priceSwapIn', () => {
+      return 'error';
+    });
+
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'SELL',
+        nonce: 21,
+        maxFeePerGas: '5000000000',
+        maxPriorityFeePerGas: '5000000000',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+
+  it('should return 500 when the priceSwapOut operation fails', async () => {
+    patchGetWallet();
+    patchInit();
+    patchStoredTokenList();
+    patchGetTokenBySymbol();
+    patchGetTokenByAddress();
+    patch(uniswap, 'priceSwapOut', () => {
+      return 'error';
+    });
+
+    await request(app)
+      .post(`/amm/trade`)
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+        quote: 'DAI',
+        base: 'WBNB',
+        amount: '10000',
+        address,
+        side: 'BUY',
+        nonce: 21,
+        maxFeePerGas: '5000000000',
+        maxPriorityFeePerGas: '5000000000',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+});
+
+describe('POST /amm/estimateGas', () => {
+  it('should return 200 for valid connector', async () => {
+    patchInit();
+    patchGasPrice();
+
+    await request(app)
+      .post('/amm/estimateGas')
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'uniswap',
+      })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((res: any) => {
+        expect(res.body.network).toEqual('mainnet');
+        expect(res.body.gasPrice).toEqual(100);
+        expect(res.body.gasCost).toEqual(
+          gasCostInEthString(100, uniswap.gasLimitEstimate)
+        );
+      });
+  });
+
+  it('should return 500 for invalid connector', async () => {
+    patchInit();
+    patchGasPrice();
+
+    await request(app)
+      .post('/amm/estimateGas')
+      .send({
+        chain: 'binance-smart-chain',
+        network: 'mainnet',
+        connector: 'pangolin',
+      })
+      .set('Accept', 'application/json')
+      .expect(500);
+  });
+});
