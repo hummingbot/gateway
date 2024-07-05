5041fc80577b380bc883737a1882cf1561567e64
Ethereal Iska <etherealiska@gmail.com>
2024-07-05 13:39:41 -0400

    Scaffolding

---
+++ src/connectors/rubicon/rubicon.config.ts
@@ -0,0 +1,17 @@
+import { AvailableNetworks } from '../../services/config-manager-types';
+
+export namespace RubiconCLOBConfig {
+  export interface NetworkConfig {
+    allowedSlippage: string;
+    tradingTypes: Array<string>;
+    chainType: string;
+    availableNetworks: Array<AvailableNetworks>;
+  }
+
+  export const config: NetworkConfig = {
+    tradingTypes: ['CLOB_SPOT'],
+    chainType: 'EVM',
+    allowedSlippage: "2/100",
+    availableNetworks: [ { chain: 'ethereum', networks: ['mainnet', 'arbitrum', 'optimism', 'base'] } ],
+  };
+}
---
+++ src/connectors/rubicon/rubicon.ts
@@ -0,0 +1,103 @@
+import { Ethereum } from '../../chains/ethereum/ethereum';
+import {
+  ClobMarketsRequest,
+  ClobOrderbookRequest,
+  ClobTickerRequest,
+  ClobGetOrderRequest,
+  ClobPostOrderRequest,
+  ClobDeleteOrderRequest,
+  ClobGetOrderResponse,
+  ClobBatchUpdateRequest,
+  CreateOrderParam,
+  ClobDeleteOrderRequestExtract,
+} from '../../clob/clob.requests';
+import {
+  CLOBish,
+  MarketInfo,
+  NetworkSelectionRequest,
+  Orderbook,
+} from '../../services/common-interfaces';
+
+export class RubiconCLOB implements CLOBish {
+  private _chain;
+  private _ready: boolean = false;
+  public parsedMarkets: MarketInfo = [];
+  public abiDecoder: any = require('abi-decoder');
+
+  private constructor(chain: string, network: string) {
+    if (chain === 'ethereum') {
+      this._chain = Ethereum.getInstance(network);
+    } else throw Error('Chain not supported.');
+  }
+
+  public async loadMarkets() {
+
+  }
+
+  public async init() {
+    if (!this._chain.ready() || Object.keys(this.parsedMarkets).length === 0) {
+      await this._chain.init();
+      await this.loadMarkets();
+      this._ready = true;
+    }
+  }
+
+  public ready(): boolean {
+    return this._ready;
+  }
+
+  public async markets(
+    req: ClobMarketsRequest
+  ): Promise<{ markets: MarketInfo }> {
+    if (req.market && req.market in this.parsedMarkets)
+      return { markets: this.parsedMarkets[req.market] };
+    return { markets: Object.values(this.parsedMarkets) };
+  }
+
+  public async orderBook(req: ClobOrderbookRequest): Promise<Orderbook> {
+    return {
+      buys: [],
+      sells: []
+    }
+  }
+
+  public async ticker(
+    req: ClobTickerRequest
+  ): Promise<{ markets: MarketInfo }> {
+    return await this.markets(req);
+  }
+
+  public async orders(
+    req: ClobGetOrderRequest
+  ): Promise<{ orders: [] }> {
+    return {
+      orders: []
+    }
+  }
+
+  public async postOrder(
+    req: ClobPostOrderRequest
+  ): Promise<{ txHash: string; clientOrderID: string }> {
+    return { txHash: "", clientOrderID: "" };
+  }
+
+  public async deleteOrder(
+    req: ClobDeleteOrderRequest
+  ): Promise<{ txHash: string }> {
+    return { txHash: "" };
+  }
+
+  public estimateGas(_req: NetworkSelectionRequest): {
+    gasPrice: number;
+    gasPriceToken: string;
+    gasLimit: number;
+    gasCost: number;
+  } {
+    return {
+      gasPrice: 0,
+      gasPriceToken: "eth",
+      gasLimit: 0,
+      gasCost: 0,
+    };
+  }
+}
