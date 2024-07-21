ce0044a941cd2370f40dd7e0122ba00ef3601062
satiparpar <mohammad.parpar.99@gmail.com>
2024-07-21 16:50:17 +0330

    add [unit-test]: Implement unit tests for ErgoController => "allowances" method

--- test-bronze/chains/ergo/ergo.controller.test.ts
+++ test-bronze/chains/ergo/ergo.controller.test.ts
@@ -296,4 +296,77 @@ describe('ErgoController', () => {
       expect(result).toEqual({});
     });
   });
+  describe('allowances', () => {
+    const request = {
+      chain: 'ergo',
+      network: 'mainnet',
+      address: 'usersPublicEthereumKey',
+      spender: 'spenderAddress',
+      tokenSymbols: [],
+    };
+    beforeEach(() => {
+      jest.spyOn(ergo, 'ready').mockReturnValue(true);
+      jest.spyOn(ergo, 'init').mockResolvedValue({} as any);
+      jest.spyOn(ergo, 'getAddressUnspentBoxes').mockResolvedValue([]);
+      jest.spyOn(ergo, 'getBalance').mockReturnValue({
+        balance: BigNumber(0),
+        assets: {},
+      });
+    });
+    it('Should be defined', () => {
+      expect(ErgoController.allowances).toBeDefined();
+    });
+    it('Should call init from ergo if ergo is not ready', async () => {
+      jest.spyOn(ergo, 'ready').mockReturnValue(false);
+      await ErgoController.allowances(ergo, request);
+      expect(ergo.init).toHaveBeenCalled();
+      expect(ergo.ready).toHaveBeenCalled();
+    });
+    it('Should not call init from ergo if ergo is ready', async () => {
+      await ErgoController.allowances(ergo, request);
+      expect(ergo.init).not.toHaveBeenCalled();
+      expect(ergo.ready).toHaveBeenCalled();
+    });
+
+    it('Should call getAddressUnspentBoxes & getBalance from ergo and return the correct data', async () => {
+      const result = await ErgoController.allowances(ergo, request);
+      expect(ergo.getAddressUnspentBoxes).toHaveBeenCalledWith(
+        'usersPublicEthereumKey',
+      );
+      expect(ergo.getBalance).toHaveBeenCalledWith([]);
+      expect(result).toMatchObject({
+        network: 'mainnet',
+        // timestamp ignored because there was a really small difference between create Date.new() in test file and main file
+        // timestamp: Date.now(),
+        latency: 0,
+        spender: 'spenderAddress',
+        approvals: { ERG: '0' },
+      });
+    });
+
+    it('Should iterate on assets returned from getBalance and return the correct data', async () => {
+      jest.spyOn(ergo, 'storedAssetList', 'get').mockReturnValue([
+        { tokenId: 'token1', decimals: 9, name: 'name', symbol: 'TKN1' },
+        { tokenId: 'token2', decimals: 3, name: 'name', symbol: 'TKN2' },
+      ]);
+
+      jest.spyOn(ergo, 'getBalance').mockReturnValue({
+        balance: BigNumber(30000000000),
+        assets: {
+          token1: BigNumber(10000000000),
+          token2: BigNumber(20000000000),
+        },
+      });
+      const result = await ErgoController.allowances(ergo, request);
+      console.log(result);
+      expect(result).toMatchObject({
+        network: 'mainnet',
+        // timestamp ignored because there was a really small difference between create Date.new() in test file and main file
+        // timestamp: Date.now(),
+        latency: 0,
+        spender: 'spenderAddress',
+        approvals: { ERG: '30', TKN1: '10', TKN2: '20000000' },
+      });
+    });
+  });
 });
