02249952b2c139aad3a634cb6cf3c53c0de3bac2
satiparpar <mohammad.parpar.99@gmail.com>
2024-07-21 11:26:56 +0330

    add [unit-test]: Implement unit tests for ErgoController => "getTokens" & "transfer" methods

--- test-bronze/chains/ergo/ergo.controller.test.ts
+++ test-bronze/chains/ergo/ergo.controller.test.ts
@@ -1,7 +1,9 @@
+import { OutputBuilder, TransactionBuilder } from '@fleet-sdk/core';
 import { Ergo } from '../../../src/chains/ergo/ergo';
 import { ErgoController } from '../../../src/chains/ergo/ergo.controller';
 import { ErgoTxFull } from '../../../src/chains/ergo/interfaces/ergo.interface';
 import { BigNumber } from 'bignumber.js';
+import { TransferRequest } from '../../../src/chains/ergo/interfaces/requests.interface';
 
 describe('ErgoController', () => {
   const ergo: Ergo = new Ergo('mainnet');
@@ -172,6 +174,126 @@ describe('ErgoController', () => {
         latency: 0,
         balances: { ERG: '30', TKN1: '10', TKN2: '20000000' },
       });
+    });
+  });
+
+  describe('getTokens', () => {
+    const mockStoredAssetList = [
+      { tokenId: 'token1', decimals: 9, name: 'name', symbol: 'TKN1' },
+      { tokenId: 'token2', decimals: 3, name: 'name', symbol: 'TKN2' },
+    ];
+    beforeEach(() => {
+      jest.spyOn(ergo, 'ready').mockReturnValue(true);
+      jest
+        .spyOn(ergo, 'storedAssetList', 'get')
+        .mockReturnValue(mockStoredAssetList);
+    });
+    it('Should be defined', () => {
+      expect(ErgoController.getTokens).toBeDefined();
+    });
+    it('Should not call init from ergo if ergo is ready', async () => {
+      jest.spyOn(ergo, 'init').mockResolvedValue({} as any);
+      await ErgoController.getTokens(ergo, {});
+      expect(ergo.init).not.toHaveBeenCalled();
+    });
+
+    it('Should call init from ergo if ergo is not ready', async () => {
+      jest.spyOn(ergo, 'ready').mockReturnValue(false);
+      jest.spyOn(ergo, 'init').mockResolvedValue({} as any);
+      await ErgoController.getTokens(ergo, {});
+
+      expect(ergo.init).toHaveBeenCalled();
+      expect(ergo.ready).toHaveBeenCalled();
+    });
+
+    it('Should return correct data', async () => {
+      const result = await ErgoController.getTokens(ergo, {});
+      expect(result).toEqual({ assets: mockStoredAssetList });
+    });
+  });
+
+  describe('transfer', () => {
+    const request: TransferRequest = {
+      fromAddress: '9huCzcjG9bmpKccftEauEYD6YP6qVpnJQXAtwuSpXy3WcMpYgFX',
+      toAddress: '9hA3BgMVWfLHTqAFbBCtHdPW7QRQDsMSQp8aaYiB6PiqVLLLAF1',
+      assets: [
+        { tokenId: 'token1', amount: BigNumber(100) },
+        { tokenId: 'token2', amount: BigNumber(200) },
+      ],
+      toValue: '100000000',
+    };
+    const mockFrom = new TransactionBuilder(1);
+    const mockTo = new TransactionBuilder(2);
+    const mockSendChangeTo = new TransactionBuilder(3);
+    const mockPayMinFee = new TransactionBuilder(4);
+
+    const utxos = [
+      {
+        value: 1000000000,
+        assets: [
+          { tokenId: 'token1', amount: 100 },
+          { tokenId: 'token2', amount: 200 },
+        ],
+      },
+      {
+        value: 2000000000,
+        assets: [{ tokenId: 'token3', amount: 300 }],
+      },
+    ];
+    it('Should be defined', () => {
+      expect(ErgoController.transfer).toBeDefined();
+    });
+
+    it('Should return correct data', async () => {
+      jest.spyOn(ergo, 'getNetworkHeight').mockResolvedValue(100);
+      jest
+        .spyOn(ergo, 'getAddressUnspentBoxes')
+        .mockResolvedValue(utxos as any);
+      jest
+        .spyOn(TransactionBuilder.prototype, 'from')
+        .mockReturnValue(mockFrom);
+      jest.spyOn(TransactionBuilder.prototype, 'to').mockReturnValue(mockTo);
+      jest
+        .spyOn(TransactionBuilder.prototype, 'sendChangeTo')
+        .mockReturnValue(mockSendChangeTo);
+      jest
+        .spyOn(TransactionBuilder.prototype, 'build')
+        .mockReturnValue({} as any);
+      jest
+        .spyOn(TransactionBuilder.prototype, 'payMinFee')
+        .mockReturnValue(mockPayMinFee);
+      jest
+        .spyOn(OutputBuilder.prototype, 'addTokens')
+        .mockReturnValue({} as any);
+      const result = await ErgoController.transfer(ergo, request);
+
+      expect(ergo.getAddressUnspentBoxes).toHaveBeenCalledWith(
+        '9huCzcjG9bmpKccftEauEYD6YP6qVpnJQXAtwuSpXy3WcMpYgFX',
+      );
+      expect(ergo.getNetworkHeight).toHaveBeenCalled();
+      expect(TransactionBuilder.prototype.from).toHaveBeenCalledWith([
+        {
+          value: '1000000000',
+          assets: [
+            { tokenId: 'token1', amount: '100' },
+            { tokenId: 'token2', amount: '200' },
+          ],
+        },
+        {
+          value: '2000000000',
+          assets: [{ tokenId: 'token3', amount: '300' }],
+        },
+      ]);
+      expect(OutputBuilder.prototype.addTokens).toHaveBeenCalledWith([
+        { tokenId: 'token1', amount: '100' },
+        { tokenId: 'token2', amount: '200' },
+      ]);
+      expect(TransactionBuilder.prototype.sendChangeTo).toHaveBeenNthCalledWith(
+        '9huCzcjG9bmpKccftEauEYD6YP6qVpnJQXAtwuSpXy3WcMpYgFX',
+      );
+      expect(TransactionBuilder.prototype.payMinFee).toHaveBeenCalled();
+      expect(TransactionBuilder.prototype.build).toHaveBeenCalled();
+      expect(result).toEqual({});
     });
   });
 });
