61825193073d7e6f7d0ccaf9bfa5e2b55c81233f
Michael Feng <mifeng@gmail.com>
2025-01-10 11:51:12 -0800

    removed larp

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,3 @@
 import { startGateway } from './app';
-import { startLarpServer } from './larp-app';
-// import { run } from '@oclif/core';
 
 startGateway();
-startLarpServer();
-
-// export const asciiLogo = `
-//  _      __    ___   ___  
-// | |    / /\\  | |_) | |_) 
-// |_|__ /_/--\\ |_| \\ |_| 
-
-// larp is a CLI/API client for on-chain liquidity providers
-// `;
-
-// if (process.env.START_SERVER === 'true') {
-//   console.log(asciiLogo);
-//   startServer().catch((error) => {
-//     console.error('Failed to start server:', error);
-//     process.exit(1);
-//   });
-// } else {
-//   run()
-//     .then(require('@oclif/core/flush'))
-//     .catch(require('@oclif/core/handle'));
-// }
--- src/larp-connectors/jupiter/README.md
+++
@@ -1,7 +0,0 @@
-# Jupiter
-
-This connector standardizes REST endpoints for interacting with [Jupiter](https://jup.ag/), a cross-chain aggregator and perpetuals exchange on the Solana blockchain.
-
-## Maintainer
-
-[fengtality](https://github.com/fengtality)
