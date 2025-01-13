3562e85474d8bc71748714a14a77a99448793823
Hudson Oliveira <hudson.oliveira@funttastic.com>
2025-01-13 17:43:03 -0300

    fixing some tests and adding new ones for ton.utils.ts

--- test-bronze/chains/ton/ton.controller.test.ts
+++ test-bronze/chains/ton/ton.controller.test.ts
@@ -1,10 +1,18 @@
 import { TonController } from '../../../src/chains/ton/ton.controller';
 import { Ton } from '../../../src/chains/ton/ton';
-import { patch } from '../../../test/services/patch';
+import { Stonfi } from '../../../src/connectors/ston_fi/ston_fi'
+import { patch, unpatch } from '../../../test/services/patch';
 import {
     AssetsRequest,
     PollRequest,
 } from '../../../src/chains/ton/ton.requests';
+import {
+    getNotNullOrThrowError,
+    getOrDefault,
+    promiseAllInBatches,
+    runWithRetryAndTimeout,
+    sleep,
+} from '../../../src/chains/ton/ton.utils';
 
 export interface BalanceRequest {
     chain: string;
@@ -14,24 +22,54 @@ export interface BalanceRequest {
 }
 
 let ton: Ton;
+let stonfi: Stonfi;
 
-const NETWORK = 'testnet';
-const MOCK_TX_HASH = 'mock-tx-hash';
+const payload = {
+    walletAddress: 'EQBSevYG7uExE6wJnWC2adG2SqcjUUqNlmeTRMFhHT3kToY',
+    queryId: 100000000374351,
+};
+const mockBase64 = Buffer.from(JSON.stringify(payload)).toString('base64');
+
+const EXPECTED_CURRENT_BLOCK_NUMBER = 100;
+const NETWORK = 'mainnet';
+const MOCK_TX_HASH = `hb-ton-stonfi-${mockBase64}`;
+console.log(MOCK_TX_HASH);
 const MOCK_ADDRESS = 'mock-address';
 const MOCK_ASSET_SYMBOL = 'TON';
 
 beforeAll(async () => {
     ton = Ton.getInstance(NETWORK);
+    stonfi = Stonfi.getInstance(NETWORK);
+
+    patch(ton, 'getTransaction', async (txHash) => {
+        expect(txHash).toBe(MOCK_TX_HASH);
+        return {
+            transaction: {
+                block: '(1,2,3)',
+                hash: MOCK_TX_HASH,
+                totalFees: '50000',
+            },
+        };
+    });
     patch(ton, 'getCurrentBlockNumber', async () => ({
-        seqno: 100,
+        seqno: EXPECTED_CURRENT_BLOCK_NUMBER,
     }));
-    patch(ton, 'getTransaction', async (_txHash: string) => ({
-        transaction: {
-            block: '(0,0,200)',
-            totalFees: '1000000000',
-        },
+    patch(stonfi, 'waitForConfirmation', jest.fn(async () => ({
+        '@type': 'Found',
+        logicalTime: '200',
         txHash: MOCK_TX_HASH,
-    }));
+        coins: '1000000000',
+    })));
+    patch(stonfi, 'getSwapStatus', jest.fn(async () => ({
+        '@type': 'Found',
+        address: 'mock-address',
+        balanceDeltas: 'mock-balance-deltas',
+        coins: '1000000000',
+        exitCode: '0',
+        logicalTime: '200',
+        queryId: 'mock-query-id',
+        txHash: MOCK_TX_HASH,
+    })));
     patch(ton, 'getAccountFromAddress', async () => ({
         publicKey: 'mock-public-key',
         secretKey: 'mock-secret-key',
@@ -62,28 +100,28 @@ beforeAll(async () => {
     patch(ton, 'optIn', async () => ({ txnID: 'mock-txn-id' }));
 
     await ton.init();
+    await stonfi.init();
 });
 
-// beforeEach(() => {
-//     unpatch();
-// });
-//
-// afterEach(() => {
-//     unpatch();
-// });
+afterAll(() => {
+    unpatch();
+});
 
 describe('TonController - poll', () => {
-    it('Should return poll response for a valid transaction', async () => {
+    it('Should return poll response with valid transaction data', async () => {
         const req: PollRequest = {
             txHash: MOCK_TX_HASH,
             network: NETWORK,
         };
 
         const response = await TonController.poll(ton, req);
-        expect(response).toHaveProperty('currentBlock', 100);
-        expect(response).toHaveProperty('txBlock', 200);
-        expect(response).toHaveProperty('txHash', MOCK_TX_HASH);
-        expect(response).toHaveProperty('fee', 1);
+
+        expect(response).toEqual({
+            currentBlock: EXPECTED_CURRENT_BLOCK_NUMBER,
+            txBlock: 3,
+            txHash: MOCK_TX_HASH,
+            fee: 0.00005,
+        });
     });
 
     it('Should throw an error if transaction is not found', async () => {
@@ -172,9 +210,110 @@ describe('TonController - getTokens', () => {
 
 describe('TonController - approve', () => {
     it('Should throw an error because the method is not implemented', async () => {
-        await expect(TonController.approve()).rejects.toThrow(
-            'The approve method is not implemented because its unnecessary.'
+        const mockRequest = {
+            network: 'mainnet',
+            address: 'mock-address',
+            assetSymbol: 'TON',
+        };
+
+        await expect(TonController.approve(mockRequest)).rejects.toThrow(
+            'Method not implemented.',
         );
     });
 });
 
+describe('Ton Utils - Unit Tests', () => {
+    it('getNotNullOrThrowError should return value if not null or undefined', () => {
+        const result = getNotNullOrThrowError('test', 'Error message');
+        expect(result).toBe('test');
+    });
+
+    it('getNotNullOrThrowError should throw error if value is null or undefined', () => {
+        expect(() => getNotNullOrThrowError(null, 'Custom error')).toThrow(
+            'Custom error'
+        );
+        expect(() => getNotNullOrThrowError(undefined)).toThrow(
+            'Value is null or undefined'
+        );
+    });
+
+    it('getOrDefault should return the value if not null or undefined', () => {
+        const result = getOrDefault('value', 'default');
+        expect(result).toBe('value');
+    });
+
+    it('getOrDefault should return the default value if value is null or undefined', () => {
+        expect(getOrDefault(null, 'default')).toBe('default');
+        expect(getOrDefault(undefined, 'default')).toBe('default');
+    });
+
+    it('sleep should delay execution for specified milliseconds', async () => {
+        const start = Date.now();
+        await sleep(100);
+        const end = Date.now();
+        expect(end - start).toBeGreaterThanOrEqual(100);
+    });
+
+    it('promiseAllInBatches should process items in batches with delay', async () => {
+        const mockTask = jest.fn(async (item) => {
+            return item; // Ensure mockTask returns the item
+        });
+        const items = [1, 2, 3, 4, 5];
+        const batchSize = 2;
+        const delayBetweenBatches = 100;
+
+        const startTime = Date.now();
+        const result = await promiseAllInBatches(mockTask, items, batchSize, delayBetweenBatches);
+
+        expect(mockTask).toHaveBeenCalledTimes(5); // Ensure task ran for each item
+        expect(result).toEqual(items); // Verify the output matches input
+
+        const endTime = Date.now();
+        expect(endTime - startTime).toBeGreaterThanOrEqual(delayBetweenBatches);
+    });
+
+    it('runWithRetryAndTimeout should retry on failure and succeed if a retry works', async () => {
+        let attempt = 0;
+        const mockFunction = jest.fn(async () => {
+            if (attempt < 2) {
+                attempt++;
+                throw new Error('Fail');
+            }
+            return 'success';
+        });
+
+        const result = await runWithRetryAndTimeout(null, mockFunction, [], 3, 100, 0);
+        expect(result).toBe('success');
+        expect(mockFunction).toHaveBeenCalledTimes(3);
+    });
+
+    it('runWithRetryAndTimeout should throw error if all retries fail', async () => {
+        const mockFunction = jest.fn(async () => {
+            throw new Error('Fail');
+        });
+
+        await expect(
+            runWithRetryAndTimeout(null, mockFunction, [], 2, 50, 0)
+        ).rejects.toThrow();
+        expect(mockFunction).toHaveBeenCalledTimes(2);
+    });
+
+    it('runWithRetryAndTimeout should throw timeout error if it exceeds timeout', async () => {
+        const mockFunction = jest.fn(
+            async () =>
+                new Promise((resolve) => setTimeout(() => resolve('success'), 200)) // Resolves after 200ms
+        );
+
+        await expect(
+            runWithRetryAndTimeout(
+                null,
+                mockFunction,
+                [], // No parameters
+                1, // 1 retry
+                0, // No delay between retries
+                100, // Timeout of 100ms (shorter than function execution time)
+                'Custom timeout error'
+            )
+        ).rejects.toThrow('Custom timeout error');
+    });
+});
