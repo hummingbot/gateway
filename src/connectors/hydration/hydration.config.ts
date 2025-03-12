726e26244fcb791331bc36faee930901f6df6d0f
Danilo Araújo Silva <danilo.silva@funttastic.com>
2025-03-12 16:46:47 -0300

    Cleaning hhydration.config.ts properties

--- src/connectors/hydration/hydration.config.ts
+++ src/connectors/hydration/hydration.config.ts
@@ -6,31 +6,11 @@ export namespace HydrationConfig {
     allowedSlippage: string;
     tradingTypes: Array<string>;
     availableNetworks: Array<AvailableNetworks>;
-    maxRetryAttempts: number;
-    retryIntervalMs: number;
-    defaultPositionPadding: number;
-    defaultFeeTier: number; // Corresponds to fee percentages (e.g., 500 = 0.05%)
-    defaultLiquidityPositionSize: number;
   }
 
   export const config: NetworkConfig = {
     allowedSlippage: ConfigManagerV2.getInstance().get(
       'hydration.allowedSlippage',
-    ),
-    maxRetryAttempts: ConfigManagerV2.getInstance().get(
-      'hydration.maxRetryAttempts',
-    ),
-    retryIntervalMs: ConfigManagerV2.getInstance().get(
-      'hydration.retryIntervalMs',
-    ),
-    defaultPositionPadding: ConfigManagerV2.getInstance().get(
-      'hydration.defaultPositionPadding',
-    ),
-    defaultFeeTier: ConfigManagerV2.getInstance().get(
-      'hydration.defaultFeeTier',
-    ),
-    defaultLiquidityPositionSize: ConfigManagerV2.getInstance().get(
-      'hydration.defaultLiquidityPositionSize',
     ),
     tradingTypes: ['AMM', 'CLMM'],
     availableNetworks: [{ chain: 'polkadot', networks: ['mainnet', 'testnet'] }],
