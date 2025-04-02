97a47e06e940656593e3518fbf18ca3d1cefb549
Michael Feng <mifeng@gmail.com>
2025-04-02 10:54:18 -0700

    fix swapPost API with fee level

--- src/connectors/jupiter/jupiter.config.ts
+++ src/connectors/jupiter/jupiter.config.ts
@@ -4,6 +4,7 @@ import { ConfigManagerV2 } from '../../services/config-manager-v2';
 export namespace JupiterConfig {
   export interface NetworkConfig {
     allowedSlippage: string;
+    priorityLevel: string;
     tradingTypes: Array<string>;
     availableNetworks: Array<AvailableNetworks>;
   }
@@ -11,6 +12,9 @@ export namespace JupiterConfig {
   export const config: NetworkConfig = {
     allowedSlippage: ConfigManagerV2.getInstance().get(
       'jupiter.allowedSlippage',
+    ),
+    priorityLevel: ConfigManagerV2.getInstance().get(
+      'jupiter.priorityLevel',
     ),
     tradingTypes: ['swap'],
     availableNetworks: [{ chain: 'solana', networks: ['mainnet-beta', 'devnet'] }],
