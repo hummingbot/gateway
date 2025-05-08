c7c072b02465ca82eb6dd35d643f1fc88ec842e3
Michael Feng <mifeng@gmail.com>
2025-05-08 12:56:16 -0700

    more reverts

--- src/services/common-interfaces.ts
+++
@@ -1,2 +0,0 @@
-// This file is kept as a placeholder for potential future interfaces
-// Common interfaces for services can be added here
--- src/services/error-handler.ts
+++
@@ -1,2 +0,0 @@
-// This file is kept as a placeholder for potential future error handlers
-// Custom error classes can be added here
