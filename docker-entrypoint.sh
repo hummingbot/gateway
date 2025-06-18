ad708458982551b02aac1f94e64f633dfcea7994
david-hummingbot <85695272+david-hummingbot@users.noreply.github.com>
2025-06-19 01:32:45 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# If /home/gateway/conf/root.yml does not exist, copy all templates
+# If /home/gateway/conf/root.yml does not exist, copy all templates (including directories)
 if [ ! -f /home/gateway/conf/root.yml ]; then
-  cp -n /home/gateway/src/templates/* /home/gateway/conf/
+  cp -r -n /home/gateway/src/templates/* /home/gateway/conf/
 fi
 
 # Execute the container's CMD
