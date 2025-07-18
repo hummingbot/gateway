d567e258e8c1a8d430146f4cc2d4eab104d872a6
WuonParticle <WuonParticle@gmail.com>
2025-07-18 14:44:43 -0600

    simplify the randomNumber on the snapshot to reduce cognitive load.

--- test/superjson-setup.ts
+++ test/superjson-setup.ts
@@ -4,7 +4,7 @@ import superjson from 'superjson';
 superjson.registerCustom<BigNumber, string>(
   {
     isApplicable: (v): v is BigNumber => BigNumber.isBigNumber(v),
-    serialize: (v) => v.toHexString(),
+    serialize: (v) => v.toString(),
     deserialize: (v) => BigNumber.from(v),
   },
   'ethers.BigNumber',
